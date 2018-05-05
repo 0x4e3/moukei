b6288a8537e414087651a3208bfcb072990731f8
Alexander Lebedev <lebedev@0x4e3.ru>
2018-05-05 18:08:13 +0300

    PEP8

--- bot.py
+++ bot.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-#import redis
-
 import moukei
 
 if __name__ == '__main__':
-  print("Create BOT")
-  bot = moukei.Bot()
-  print("run BOT")
-  bot.run()
+    print("Create BOT")
+    bot = moukei.Bot()
+    print("run BOT")
+    bot.run()
